ecf38ef320b160c9828a918fa9080055fc19a6b3
Claquettes <math@claq.fr>
2023-05-23 15:25:34 +0200

    data

---
+++ js/data.js
@@ -0,0 +1,89 @@
+const postInfos = [
+    {
+        id: 1,
+        language: "Js",
+        visible: true,
+        year : 2022
+    },
+    {
+        id: 2,
+        language: "Js",
+        visible: true,
+        year : 2022
+
+    },
+    {
+        id: 3,
+        language: "Js",
+        visible: true,
+        year : 2022
+
+    },
+    {
+        id: 4,
+        language: "Js",
+        visible: true,
+        year : 2023
+    },
+    {
+        id: 5,
+        language: "Js", 
+        visible: true,
+        year : 2023
+    },
+    {
+        id: 6,
+        language: "Ts",
+        visible: true,
+        year : 2023
+    },
+    {
+        id: 7,
+        language: "Js",
+        visible: true,
+        year : 2023
+    },
+    {
+        id: 8,
+        language: "Js",
+        visible: true,
+        year : 2023
+    },
+    {
+        id: 9,
+        language: "Python",
+        visible: true,
+        year : 2023
+    },
+    {
+        id: 10,
+        language: "Python",
+        visible: true,
+        year : 2023
+    },
+    {
+        id: 11,
+        language: "Python",
+        visible: true,
+        year : 2023
+    },
+    {
+        id: 12,
+        language: "Js",
+        visible: true,
+        year : 2023
+    },
+    {
+        id: 13,
+        language: "Js",
+        visible: true,
+        year : 2023
+    },
+    {
+        id: 14,
+        language: "Cpp",
+        visible: true,
+        year : 2023
+    }
+
+];
