4b95c2690f760c8a37107c8c5dfdb05b19d0b84d
Claq <math@claq.fr>
2023-05-30 21:51:20 +0200

    19 js

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-const postArray = [18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const postArray = [19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
 const postInfos = [
     {
@@ -128,5 +128,12 @@ const postInfos = [
         visible: true,
         year : 2023,
         scolar : true
+    },
+    {
+        id: 19,
+        language: "Js",
+        visible: true,
+        year : 2023,
+        scolar : false
     }
 ];
