aeb9ab75268ee27bcadfabd30015d511b4b83755
Claq <math@claq.fr>
2023-06-26 14:25:08 +0200

    ADDED 23 SANDBOX

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-const postArray = [22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const postArray = [23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
 const postInfos = [
     {
@@ -152,6 +152,13 @@ const postInfos = [
     },
     {
         id: 22,
+        language: "Js",
+        visible: true,
+        year : 2023,
+        scolar : false
+    },
+    {
+        id: 23,
         language: "Js",
         visible: true,
         year : 2023,
