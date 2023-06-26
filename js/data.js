b53b2f35f7cec08bceceb0ff4f83c86d5333dad0
Claq <math@claq.fr>
2023-06-26 12:31:12 +0200

    added rythm and cookin 21 and 22

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-const postArray = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const postArray = [22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
 const postInfos = [
     {
@@ -142,6 +142,19 @@ const postInfos = [
         visible: true,
         year : 2023,
         scolar : false
+    },
+    {
+        id: 21,
+        language: "Cpp",
+        visible: true,
+        year : 2023,
+        scolar : false
+    },
+    {
+        id: 22,
+        language: "Js",
+        visible: true,
+        year : 2023,
+        scolar : false
     }
-
 ];
