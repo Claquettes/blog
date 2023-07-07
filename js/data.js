3ef513a9e03c66a00e4d84da367616b59ccb7e85
Claq <math@claq.fr>
2023-07-07 22:43:14 +0200

    added 24 (fzu site)

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-const postArray = [23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const postArray = [24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
 const postInfos = [
     {
@@ -159,6 +159,13 @@ const postInfos = [
     },
     {
         id: 23,
+        language: "Js",
+        visible: true,
+        year : 2023,
+        scolar : false
+    },
+    {
+        id: 24,
         language: "Js",
         visible: true,
         year : 2023,
